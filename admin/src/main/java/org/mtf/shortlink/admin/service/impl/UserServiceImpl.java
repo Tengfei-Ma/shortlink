d0347c14eaa50e96e1c064b92a4e1f49e37b05a2
mtf <m1501440483@email.swu.edu.cn>
2024-04-02 17:37:00 +0800

    通过分布式锁完善用户注册功能，防止使用相同的用户名同时进行注册

--- admin/src/main/java/org/mtf/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/mtf/shortlink/admin/service/impl/UserServiceImpl.java
@@ -13,8 +13,13 @@ import org.mtf.shortlink.admin.dto.req.UserRegisterReqDTO;
 import org.mtf.shortlink.admin.dto.resp.UserRespDTO;
 import org.mtf.shortlink.admin.service.UserService;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+import static org.mtf.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 
 /**
  * 用户接口实现层
@@ -23,7 +28,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
-
+    private final RedissonClient redissonClient;
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class).eq(UserDO::getUsername, username);
@@ -46,10 +51,20 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if(!hasUsername(requestParam.getUsername())){
             throw new ClientException(UserErroeCodeEnum.USER_NAME_EXIST);
         }
-        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if(inserted<1){
-            throw new ClientException(UserErroeCodeEnum.USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY);
+        if(!lock.tryLock()){
+            throw new ClientException(UserErroeCodeEnum.USER_NAME_EXIST);
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+        try{
+            int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+            if(inserted<1){
+                throw new ClientException(UserErroeCodeEnum.USER_SAVE_ERROR);
+            }
+            userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+        }catch (DuplicateKeyException exception){
+            throw new ClientException(UserErroeCodeEnum.User_EXIST);
+        }finally {
+            lock.unlock();
+        }
     }
 }
