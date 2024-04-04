d1e4a16def2b340bd153cdfbdd15f2b5fed56c8a
mtf <m1501440483@email.swu.edu.cn>
2024-04-04 17:26:01 +0800

    移植通用类

---
+++ project/src/main/java/org/mtf/shortlink/project/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,12 @@
+package org.mtf.shortlink.project.common.convention.errorcode;
+
+public interface IErrorCode {
+    /**
+     * 错误码
+     */
+    String code();
+    /**
+     * 错误信息
+     */
+    String message();
+}
