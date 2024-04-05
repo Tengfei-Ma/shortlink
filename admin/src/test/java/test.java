8ab172fe1ee0f09ac7f428d701a2ac7d2645249e
mtf <m1501440483@email.swu.edu.cn>
2024-04-05 15:07:23 +0800

    对短链接分组表分表

--- admin/src/test/java/test.java
+++ admin/src/test/java/test.java
@@ -1,20 +1,18 @@
 public class test {
     public static void main(String[] args) {
-        String sql="CREATE TABLE `t_user_%d` (\n" +
-                "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-                "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
-                "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
-                "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
-                "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
-                "  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
-                "  `deletion_time` bigint DEFAULT NULL COMMENT '注销时间戳',\n" +
-                "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
-                "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
-                "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '是否删除 0：未删除 1：已删除',\n" +
+        String sql="CREATE TABLE `link`.`t_group_%d` (\n" +
+                "  `id` BIGINT(20) NOT NULL COMMENT 'ID',\n" +
+                "  `gid` VARCHAR(32) NULL COMMENT '分组标识',\n" +
+                "  `name` VARCHAR(64) NULL COMMENT '分组名称',\n" +
+                "  `username` VARCHAR(256) NULL COMMENT '创建分组用户名',\n" +
+                "  `sort_order` INT(3) NULL COMMENT '排序标识',\n" +
+                "  `create_time` DATETIME NULL COMMENT '创建时间',\n" +
+                "  `update_time` DATETIME NULL COMMENT '修改时间',\n" +
+                "  `del_flag` TINYINT(1) NULL COMMENT '是否删除 0：未删除 1：已删除',\n" +
                 "  PRIMARY KEY (`id`),\n" +
-                "  UNIQUE KEY `idx_unique_username` (`username`) USING BTREE\n" +
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表'";
-        for (int i = 0; i < 15; i++) {
+                "  UNIQUE INDEX `idx_unique_username_gid` USING BTREE (`gid`, `username`) VISIBLE\n" +
+                ") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='短链接分组表';";
+        for (int i = 0; i < 16; i++) {
             System.out.printf((sql) + "%n",i);
         }
     }
