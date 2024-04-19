1999b78f12c7cf89fdea7509efe35b3a60777348
mtf <m1501440483@email.swu.edu.cn>
2024-04-19 11:57:31 +0800

    创建分组添加分布式锁并判断是否超过最大分组数

--- admin/src/main/java/org/mtf/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/org/mtf/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -2,7 +2,7 @@ package org.mtf.shortlink.admin.common.enums;
 
 import org.mtf.shortlink.admin.common.convention.errorcode.IErrorCode;
 
-public enum UserErroeCodeEnum implements IErrorCode {
+public enum UserErrorCodeEnum implements IErrorCode {
 
     USER_NULL("B000200","用户记录不存在"),
     USER_NAME_EXIST("B000201","用户名已存在"),
@@ -12,14 +12,15 @@ public enum UserErroeCodeEnum implements IErrorCode {
     USER_LOGIN_ERROR("B000205","用户登陆错误"),
     USER_NOT_LOGIN("B000206","用户未登录"),
     USERNAME_OR_TOKEN__NOT_EXIST("B000207","用户名或token不存在"),
-    USER_GROUP_NOT_EXIST("B000208","用户无分组信息");
+    USER_GROUP_NOT_EXIST("B000208","用户无分组信息"),
+    USER_GROUP_OVER_MAX("B000209","分组数超出限制");
 
 
     private final String code;
 
     private final String message;
 
-    UserErroeCodeEnum(String code, String message) {
+    UserErrorCodeEnum(String code, String message) {
         this.code = code;
         this.message = message;
     }
--- admin/src/main/java/org/mtf/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/org/mtf/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,11 +1,14 @@
 package org.mtf.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.mtf.shortlink.admin.common.biz.user.UserContext;
+import org.mtf.shortlink.admin.common.convention.exception.ClientException;
 import org.mtf.shortlink.admin.dao.entity.GroupDO;
 import org.mtf.shortlink.admin.dao.mapper.GroupMapper;
 import org.mtf.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
@@ -15,16 +18,28 @@ import org.mtf.shortlink.admin.remote.ShortlinkRemoteService;
 import org.mtf.shortlink.admin.remote.dto.resp.ShortlinkGroupCountRespDTO;
 import org.mtf.shortlink.admin.service.GroupService;
 import org.mtf.shortlink.admin.toolkit.RandomGenerator;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+import static org.mtf.shortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
+import static org.mtf.shortlink.admin.common.enums.UserErrorCodeEnum.USER_GROUP_OVER_MAX;
+
 /**
  * 短链接分组接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
+    private final RedissonClient redissonClient;
+
+    @Value("${short-link.group.max-num}")
+    private Integer groupMaxNum;
+
     private ShortlinkRemoteService shortlinkRemoteService = new ShortlinkRemoteService() {
     };
 
@@ -35,16 +50,29 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public void saveGroup(String username, String name) {
-        String gid;
-        do {
-            gid = RandomGenerator.generateRandom();
-        } while (!hasGid(username, gid));
-        GroupDO groupDO = new GroupDO();
-        groupDO.setGid(RandomGenerator.generateRandom());
-        groupDO.setName(name);
-        groupDO.setUsername(username);
-        groupDO.setSortOrder(0);
-        baseMapper.insert(groupDO);
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+                throw new ClientException(USER_GROUP_OVER_MAX);
+            }
+            String gid;
+            do {
+                gid = RandomGenerator.generateRandom();
+            } while (!hasGid(username, gid));
+            GroupDO groupDO = new GroupDO();
+            groupDO.setGid(RandomGenerator.generateRandom());
+            groupDO.setName(name);
+            groupDO.setUsername(username);
+            groupDO.setSortOrder(0);
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
