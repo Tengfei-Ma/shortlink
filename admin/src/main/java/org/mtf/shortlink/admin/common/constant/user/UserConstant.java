7d8eb8365be96dad67c454fa49abc0b79afaffc5
mtf <m1501440483@email.swu.edu.cn>
2024-04-20 10:45:10 +0800

    删除未使用字段

--- admin/src/main/java/org/mtf/shortlink/admin/common/constant/user/UserConstant.java
+++ admin/src/main/java/org/mtf/shortlink/admin/common/constant/user/UserConstant.java
@@ -3,6 +3,5 @@ package org.mtf.shortlink.admin.common.constant.user;
 public class UserConstant {
     public static final String USERINFO_USERNAME = "username";
     public static final String USERINFO_TOKEN = "token";
-    public static final String USERINFO_DETAIL = "realName";
 
 }
